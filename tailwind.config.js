d464a0a6730f493005fd2336306f7fc281debc6f
nirob <nirobsarker012@gmail.com>
2024-11-23 19:04:32 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      container:{
+        center:true,
+        padding: "20px",
+      },
+
+      screens:{
+        "sm": "576px",
+        "md" : "768px",
+        "lg": "992px",
+        "xl": "1200px",
+        "2xl" : "1490px",
+      },
+
+      fontFamily:{
+        viga:["Viga", "sans-serif"],
+        pt:["PT Serif", "serif"],
+      }
+    },
+  },
+  plugins: [],
+}
